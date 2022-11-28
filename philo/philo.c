bfe4d784f42bc4e8d177fe2296ab2b49be9ce082
Much3m <kga1800@uos.ac.kr>
2022-11-28 23:38:07 +0900

    eat time mutex modified

--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: min-skim <min-skim@student.42seou.kr>      +#+  +:+       +#+        */
+/*   By: min-skim <min-skim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:13:06 by min-skim          #+#    #+#             */
-/*   Updated: 2022/11/28 17:19:52 by min-skim         ###   ########.fr       */
+/*   Updated: 2022/11/28 21:58:46 by min-skim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ int	count_meals(t_philo *philo)
 		i = -1;
 		while (++i < philo->philo_num)
 		{
-			pthread_mutex_lock(&philo->eat_count);
+			pthread_mutex_lock(&philo[i].eat_count);
 			if (philo[i].total_count_eat < philo->total_count_eat_2)
 				flag = 0;
-			pthread_mutex_unlock(&philo->eat_count);
+			pthread_mutex_unlock(&philo[i].eat_count);
 		}
 		if (flag == 1)
 		{
